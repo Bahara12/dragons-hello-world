37c43daaf9484b9cff43677b5d8de6092e36c38f
bahara Niazi <baharniazi100@gmail.com>
2023-05-17 22:36:13 -0400

    my first commit

--- src/com/hello/HelloWorld.java
+++ src/com/hello/HelloWorld.java
@@ -3,6 +3,6 @@ package com.hello;
 public class HelloWorld {
 
 	public static void main(String [] args) {
-		System.out.println("hello world this time from Git and GitHub");
+		System.out.println("hellosss world this time from Git and GitHub");
 	}
 }
